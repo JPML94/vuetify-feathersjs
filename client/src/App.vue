c3291baa3c06bd78238f5c96e7d8953548107005
JPML94 <jp@tradecrafted.com>
2018-06-04 17:55:04 -0700

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-toolbar
+      app
+    >
+    <v-toolbar-title>Vuetify - FeathersJS</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items v-if="!user">
+      <v-btn flat :to="{ name: 'login' }">Go to login</v-btn>
+      <v-btn flat :to="{ name: 'signup' }">SignUp</v-btn>
+    </v-toolbar-items>
+    <v-toolbar-items v-if="user">
+      <v-btn flat @click="logout">LogOut</v-btn>
+    </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018 - made with ♥ by JP</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+export default {
+  name: 'App',
+  data() {
+    return {
+      fixed: false,
+    };
+  },
+  computed: {
+    ...mapState('auth', {user: 'payload'})
+  },
+  methods: {
+    ...mapActions('auth', { authLogout: 'logout'}),
+    logout() {
+      this.authLogout().then(() => this.$router.push('/login'))
+    }
+  }
+};
+</script>
---
+++ client/src/router.js
@@ -0,0 +1,49 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import SignUp from './views/SignUp.vue';
+import Login from './views/Login.vue';
+import Boards from './views/Boards.vue';
+
+import store from './store';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      beforeEnter(to, from, next) {
+        store.dispatch('auth/authenticate').then(() => {
+          next('/boards');
+        }).catch(() => {
+          next('/login');
+        });
+      },
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/boards',
+      name: 'boards',
+      component: Boards,
+      beforeEnter(to, from, next) {
+        store.dispatch('auth/authenticate').then(() => {
+          next();
+        }).catch(() => {
+          next('/login');
+        });
+      },
+    },
+  ],
+});
